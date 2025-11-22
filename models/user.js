b6043bc8757bc14d999a57e9befd5c2920ea1b1e
MCegla-JW <m.cegla93@gmail.com>
2025-11-22 16:34:46 +0000

    created sign-in and sign-up routes, added password removal from JSON rep of doc sent back to client

--- models/user.js
+++ models/user.js
@@ -1,32 +1,38 @@
 import mongoose from 'mongoose'
 import bcrypt from 'bcrypt'
 
-const userSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true}, 
-    email: { type: String, required: true, unique: true},
-    password: { type: String, required: true, unique: true},
+const userSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  {
+    toJSON: {
+      transform: (doc, ret) => {
+        delete ret.password
+        return ret
+      },
+    },
+  },
+)
+
+userSchema.virtual('confirmPassword').set(function (passwordValue) {
+  this._confirmPassword = passwordValue
 })
 
-userSchema
-.virtual('confirmPassword')
-.set(function(passwordValue) {
-    this._confirmPassword = passwordValue
+userSchema.pre('validate', function () {
+  if (this.isModified('password') && this.password !== this._confirmPassword) {
+    this.invalidate('confirmPassword', 'Passwords do not match')
+  }
 })
 
-userSchema.pre('validate', function(next){
-    if (this.isModified('password') && this.password !== this._confirmPassword){
-        this.invalidate('confirmPassword', 'Passwords do not match')
-    }
-    next()
-})
-
-userSchema.pre('save', function(next){
-    if (this.isModified('password')){
+userSchema.pre('save', function () {
+  if (this.isModified('password')) {
     this.password = bcrypt.hashSync(this.password, 12)
-    }
-    next()
+  }
 })
 
 const User = mongoose.model('User', userSchema)
 
-export default User 
+export default User
