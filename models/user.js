eeb27730087119bd6533143c167e4538d5c855a4
MCegla-JW <m.cegla93@gmail.com>
2025-11-22 15:01:29 +0000

    created password hashing hook

--- models/user.js
+++ models/user.js
@@ -4,7 +4,14 @@ import bcrypt from 'bcrypt'
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true}, 
     email: { type: String, required: true, unique: true},
-    password: { type: String, required: true, unique: true}
+    password: { type: String, required: true, unique: true},
+})
+
+userSchema.pre('save', function(next){
+    if (this.isModified('password')){
+    this.password = bcrypt.hashSync(this.password, 12)
+    }
+    next()
 })
 
 const User = mongoose.model('User', userSchema)
